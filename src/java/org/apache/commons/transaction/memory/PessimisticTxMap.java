dc66aa3cbb29df21dcdccdb517eeb3eb6e1c1026
Oliver Zeigermann <ozeigermann@apache.org>
2007-07-20 16:13:20 +0000

    Reintroduces transactional memory implementations. However, they no longer wrap a map, but hide the wrapped map as an implementation detail.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/transaction/branches/TRANSACTION_2@558031 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/transaction/memory/PessimisticTxMap.java
@@ -0,0 +1,147 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.transaction.memory;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.transaction.TransactionalResourceManager;
+
+/**
+ * Wrapper that adds transactional control to all kinds of maps that implement
+ * the {@link Map} interface. By using pessimistic transaction control (blocking
+ * locks) this wrapper has better isolation than {@link TxMap},
+ * but also has less possible concurrency and may even deadlock. A commit,
+ * however, will never fail. <br>
+ * Start a transaction by calling {@link #startTransaction()}. Then perform the
+ * normal actions on the map and finally either call
+ * {@link #commitTransaction()} to make your changes permanent or
+ * {@link #rollbackTransaction()} to undo them. <br>
+ * <em>Caution:</em> Do not modify values retrieved by {@link #get(Object)} as
+ * this will circumvent the transactional mechanism. Rather clone the value or
+ * copy it in a way you see fit and store it back using
+ * {@link #put(Object, Object)}. <br>
+ * <em>Note:</em> This wrapper guarantees isolation level
+ * <code>SERIALIZABLE</code>.
+ * 
+ * @version $Id: PessimisticMapWrapper.java 493628 2007-01-07 01:42:48Z joerg $
+ * @see TxMap
+ * @see OptimisticTxMap
+ */
+public class PessimisticTxMap<K, V> extends TxMap<K, V> implements Map<K, V>,
+TransactionalResourceManager {
+
+    protected static final Object GLOBAL_LOCK = "GLOBAL";
+
+    public Collection values() {
+        assureGlobalReadLock();
+        return super.values();
+    }
+
+    public Set entrySet() {
+        assureGlobalReadLock();
+        return super.entrySet();
+    }
+
+    public Set keySet() {
+        assureGlobalReadLock();
+        return super.keySet();
+    }
+
+    public V remove(Object key) {
+        // assure we get a write lock before super can get a read lock to avoid
+        // lots
+        // of deadlocks
+        assureWriteLock(key);
+        return super.remove(key);
+    }
+
+    public V put(K key, V value) {
+        // assure we get a write lock before super can get a read lock to avoid
+        // lots
+        // of deadlocks
+        assureWriteLock(key);
+        return super.put(key, value);
+    }
+
+    protected void assureWriteLock(Object key) {
+        LockingTxContext txContext = (LockingTxContext) getActiveTx();
+        if (txContext != null) {
+            txContext.writeLock(key);
+            // XXX fake intention lock (prohibits global WRITE)
+            txContext.readLock(GLOBAL_LOCK);
+        }
+    }
+
+    protected void assureGlobalReadLock() {
+        LockingTxContext txContext = (LockingTxContext) getActiveTx();
+        if (txContext != null) {
+            // XXX fake intention lock (prohibits global WRITE)
+            txContext.readLock(GLOBAL_LOCK);
+        }
+    }
+
+    @Override
+    protected LockingTxContext createContext() {
+        return new LockingTxContext();
+    }
+
+    public class LockingTxContext extends MapTxContext {
+
+        protected Set keys() {
+            readLock(GLOBAL_LOCK);
+            return super.keys();
+        }
+
+        protected V get(Object key) {
+            readLock(key);
+            // XXX fake intention lock (prohibits global WRITE)
+            readLock(GLOBAL_LOCK);
+            return super.get(key);
+        }
+
+        protected void put(K key, V value) {
+            writeLock(key);
+            // XXX fake intention lock (prohibits global WRITE)
+            readLock(GLOBAL_LOCK);
+            super.put(key, value);
+        }
+
+        protected void remove(Object key) {
+            writeLock(key);
+            // XXX fake intention lock (prohibits global WRITE)
+            readLock(GLOBAL_LOCK);
+            super.remove(key);
+        }
+
+        protected int size() {
+            // XXX this is bad luck, we need a global read lock just for the
+            // size :( :( :(
+            readLock(GLOBAL_LOCK);
+            return super.size();
+        }
+
+        protected void clear() {
+            writeLock(GLOBAL_LOCK);
+            super.clear();
+        }
+
+    }
+
+}
