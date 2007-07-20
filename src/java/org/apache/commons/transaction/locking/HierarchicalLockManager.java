e5704b8ae5cf5731ddbfba9c1a4a91747824a8a1
Oliver Zeigermann <ozeigermann@apache.org>
2007-07-20 16:20:20 +0000

    New to manager hierarchical locking as required by file resource manager.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/transaction/branches/TRANSACTION_2@558040 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/transaction/locking/HierarchicalLockManager.java
@@ -0,0 +1,7 @@
+package org.apache.commons.transaction.locking;
+
+
+// Takes care of hierarchical locking with folders and resources
+public class HierarchicalLockManager extends ReadWriteLockManager {
+
+}
